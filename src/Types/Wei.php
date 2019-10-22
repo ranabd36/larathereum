aa691166f5245c2f467c8c988ed881feadf9fc14
Moh. Ibrahim Al Naz Rana <ranabd36@gmail.com>
2019-10-22 15:51:31 +0600

    Update Wei construct

--- src/Types/Wei.php
+++ src/Types/Wei.php
@@ -8,7 +8,22 @@ class Wei
 
     public function __construct($amount)
     {
-        $this->amount = (string)$amount;
+        $this->amount = $this->format($amount);
+    }
+
+    private function format($amount)
+    {
+        $amount = (string)$amount;
+        if (preg_match('/e|E/', $amount, $match)) {
+            $comps = explode($match[0], $amount);
+            if (count($comps) > 2 || count($comps) < 2 ) {
+                throw new \Exception('Invalid scientific notation.');
+            }
+
+            return str_pad(intval($comps[0]), $comps[1], '0', STR_PAD_RIGHT);
+        }
+
+        return $amount;
     }
 
     public function amount(): string
